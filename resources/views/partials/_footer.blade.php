35bcede0b3343e1940a0c0f5333b55a1bec6b775
HiranyaRanawaka99 <hiranya.ranawaka99@gmail.com>
2023-10-14 18:51:29 +0300

    add: footer-bottom

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -15,3 +15,27 @@
         <img src="{{ url('/images/dc-logo-bg.png')}}" alt="">
     </div>
 </div>
+
+<div class="footerBottom wrapper">
+      <button class="sign-up-btn">
+        SIGN-UP NOW!
+      </button>
+    <div class="social">
+      <p>FOLLOW US</p>
+        <div 
+          class="social-icons d-flex gap-2">
+          <a href="#">
+            <img src=" {{ url('/images/footer-facebook.png')}}">
+         </a>
+         <a href="#">
+            <img src=" {{ url('/images/footer-pinterest.png')}}">
+         </a>
+         <a href="#">
+            <img src=" {{ url('/images/footer-twitter.png')}}">
+         </a>
+         <a href="#">
+            <img src=" {{ url('/images/footer-youtube.png')}}">
+         </a>
+        </div>
+    </div>
+  </div>
